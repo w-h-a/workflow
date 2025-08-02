b958a43fc3d9ccc2f86c9d4f27e2292d36116740
Wes Anderson <wesley.herb.anderson@gmail.com>
2025-08-02 07:54:51 -0700

    feat: get it working somewhat

---
+++ internal/engine/clients/runner/options.go
@@ -0,0 +1,113 @@
+package runner
+
+import "context"
+
+type Option func(o *Options)
+
+type Options struct {
+	Host    string
+	Context context.Context
+}
+
+func WithHost(host string) Option {
+	return func(o *Options) {
+		o.Host = host
+	}
+}
+
+func NewOptions(opts ...Option) Options {
+	options := Options{
+		Context: context.Background(),
+	}
+
+	for _, fn := range opts {
+		fn(&options)
+	}
+
+	return options
+}
+
+type StopOption func(o *StopOptions)
+
+type StopOptions struct {
+	ID      string
+	Context context.Context
+}
+
+func StopWithID(id string) StopOption {
+	return func(o *StopOptions) {
+		o.ID = id
+	}
+}
+
+func NewStopOptions(opts ...StopOption) StopOptions {
+	options := StopOptions{
+		Context: context.Background(),
+	}
+
+	for _, fn := range opts {
+		fn(&options)
+	}
+
+	return options
+}
+
+type StartOption func(o *StartOptions)
+
+type StartOptions struct {
+	ID            string
+	Name          string
+	Image         string
+	Memory        int64
+	Env           []string
+	RestartPolicy string
+	Context       context.Context
+}
+
+func StartWithID(id string) StartOption {
+	return func(o *StartOptions) {
+		o.ID = id
+	}
+}
+
+func StartWithName(name string) StartOption {
+	return func(o *StartOptions) {
+		o.Name = name
+	}
+}
+
+func StartWithImage(image string) StartOption {
+	return func(o *StartOptions) {
+		o.Image = image
+	}
+}
+
+func StartWithMemory(memory int64) StartOption {
+	return func(o *StartOptions) {
+		o.Memory = memory
+	}
+}
+
+func StartWithEnv(env []string) StartOption {
+	return func(o *StartOptions) {
+		o.Env = env
+	}
+}
+
+func StartWithRestartPolicy(policy string) StartOption {
+	return func(o *StartOptions) {
+		o.RestartPolicy = policy
+	}
+}
+
+func NewStartOptions(opts ...StartOption) StartOptions {
+	options := StartOptions{
+		Context: context.Background(),
+	}
+
+	for _, fn := range opts {
+		fn(&options)
+	}
+
+	return options
+}
--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/w-h-a/workflow/internal/engine/clients/runner"
+	"github.com/w-h-a/workflow/internal/engine/clients/runner/docker"
+	"github.com/w-h-a/workflow/internal/engine/services/worker"
+	"github.com/w-h-a/workflow/internal/task"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	runnerClient := docker.NewRunner(
+		runner.WithHost("unix:///Users/wesleyanderson/.docker/run/docker.sock"),
+	)
+
+	w := worker.New(runnerClient)
+
+	t := task.Task{
+		ID:    uuid.New().String(),
+		State: task.Pending,
+		Name:  "test-container-1",
+		Image: "postgres:13",
+		Env: []string{
+			"POSTGRES_USER=user",
+			"POSTGRES_PASSWORD=secret",
+		},
+	}
+
+	w.EnqueueTask(context.Background(), t)
+
+	if err := w.RunTask(context.Background()); err != nil {
+		panic(err)
+	}
+
+	time.Sleep(10 * time.Second)
+
+	if err := w.StopTask(context.Background(), t); err != nil {
+		panic(err)
+	}
 }
