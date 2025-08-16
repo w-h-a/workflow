ed0a2ef06fd08b335a306fd523941246e2d60015
Wes Anderson <wesley.herb.anderson@gmail.com>
2025-08-16 16:47:15 -0700

    refactor: use multiple connections for rabbit impl (#25)
    Refactors the RabbitMQ broker impl to use separate connections for subscribing and publishing. Additionally, introduces a QOS setting for consumers to better manage flow and prevent consumer overload.

--- internal/engine/clients/broker/errors.go
+++ internal/engine/clients/broker/errors.go
@@ -3,8 +3,9 @@ package broker
 import "errors"
 
 var (
-	ErrCreatingChannel = errors.New("failed to create channel")
-	ErrCreatingQueue   = errors.New("failed to create queue")
-	ErrSubscribing     = errors.New("failed to subscribe")
-	ErrPublishing      = errors.New("failed to publish")
+	ErrCreatingChannel     = errors.New("failed to create channel")
+	ErrCreatingQueue       = errors.New("failed to create queue")
+	ErrSettingMessageCount = errors.New("failed to set message count")
+	ErrSubscribing         = errors.New("failed to subscribe")
+	ErrPublishing          = errors.New("failed to publish")
 )
