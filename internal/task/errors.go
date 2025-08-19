61db241ff2acbdd8ab7dc6cc6c195e8e33dc1ad7
Wes Anderson <wesley.herb.anderson@gmail.com>
2025-08-19 07:22:27 -0700

    refactor: cleanup some things (#33)

--- internal/task/errors.go
+++ internal/task/errors.go
@@ -4,6 +4,7 @@ import "errors"
 
 var (
 	ErrTaskNotFound                  = errors.New("task not found")
+	ErrMissingImage                  = errors.New("task is missing image")
 	ErrAttemptsSpecified             = errors.New("may not specify retry attempts")
 	ErrExcessiveLimit                = errors.New("may not specify retry limit > 10")
 	ErrInvalidInitialDelayDuration   = errors.New("invalid initial delay duration")
