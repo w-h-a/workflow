b715f1a04819e2e25bf74723ce6f2e9f275fa680
Wes Anderson <wesley.herb.anderson@gmail.com>
2025-08-18 17:17:15 -0700

    feat: introduce retry

--- internal/task/errors.go
+++ internal/task/errors.go
@@ -3,5 +3,9 @@ package task
 import "errors"
 
 var (
-	ErrTaskNotFound = errors.New("task not found")
+	ErrTaskNotFound                  = errors.New("task not found")
+	ErrAttemptsSpecified             = errors.New("may not specify retry attempts")
+	ErrExcessiveLimit                = errors.New("may not specify retry limit > 10")
+	ErrInvalidInitialDelayDuration   = errors.New("invalid initial delay duration")
+	ErrExcessiveInitialDelayDuration = errors.New("may not specify retry initial delay duration > 5 mins")
 )
