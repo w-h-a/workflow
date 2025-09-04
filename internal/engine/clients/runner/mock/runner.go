81c6f6f46ca65934fcb18bae6c45c4796127b5fe
Wes Anderson <wesley.herb.anderson@gmail.com>
2025-09-03 17:05:19 -0700

    tests: add mock runner and special method on in memory broker for tests to come

--- internal/engine/clients/runner/mock/runner.go
+++ internal/engine/clients/runner/mock/runner.go
@@ -1,1 +1,41 @@
 package mock
+
+import (
+	"context"
+
+	testmock "github.com/stretchr/testify/mock"
+	"github.com/w-h-a/workflow/internal/engine/clients/runner"
+)
+
+type mockRunner struct {
+	testmock.Mock
+}
+
+func (r *mockRunner) Run(ctx context.Context, opts ...runner.RunOption) (string, error) {
+	args := r.Called(ctx, opts)
+	return args.String(0), args.Error(1)
+}
+
+func (r *mockRunner) CreateVolume(ctx context.Context, opts ...runner.CreateVolumeOption) error {
+	args := r.Called(ctx, opts)
+	return args.Error(0)
+}
+
+func (r *mockRunner) DeleteVolume(ctx context.Context, opts ...runner.DeleteVolumeOption) error {
+	args := r.Called(ctx, opts)
+	return args.Error(0)
+}
+
+func (r *mockRunner) CheckHealth(ctx context.Context) error {
+	args := r.Called(ctx)
+	return args.Error(0)
+}
+
+func (r *mockRunner) Close(ctx context.Context) error {
+	args := r.Called(ctx)
+	return args.Error(0)
+}
+
+func NewRunner(opts ...runner.Option) runner.Runner {
+	return &mockRunner{}
+}
